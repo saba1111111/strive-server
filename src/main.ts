590545598a3d5778f8548607f69acaf09f6fc8ad
Saba Pachulia <sabapachulia@omegas-MacBook-Pro.local>
2025-05-09 11:09:45 +0400

    fix cors problem

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['https://saba1111111.github.io', 'https://duolingo-dream-launchpad.lovable.app/', 'http://localhost:5173'],
+    credentials: true,
+  });
 
   const config = new DocumentBuilder().setTitle('Strive API').build();
 
